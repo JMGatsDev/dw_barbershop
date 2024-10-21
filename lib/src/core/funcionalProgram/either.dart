28a88b9d71e088d244e820ca1c07d09c4b3067a7
João Marcos <joaogatis.hrzon@gmail.com>
2024-10-21 16:05:31 -0300

    refactor: mudando a posição do success

--- lib/src/core/funcionalProgram/either.dart
+++ lib/src/core/funcionalProgram/either.dart
@@ -1,11 +1,11 @@
 sealed class Either<E extends Exception, S> {}
 
-class Failure<E extends Exception, S> extends Either<E, S> {
-  Failure({required this.exception});
-  final E exception;
-}
-
 class Success<E extends Exception, S> extends Either<E, S> {
   Success({required this.value});
   final S value;
+}
+
+class Failure<E extends Exception, S> extends Either<E, S> {
+  Failure({required this.exception});
+  final E exception;
 }
