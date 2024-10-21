03a672de975dd76b66c1796274eae33bfec7c5fb
João Marcos <joaogatis.hrzon@gmail.com>
2024-10-21 16:03:56 -0300

    refactor: corrigindo

--- lib/src/core/ui/helpers/form_helpers.dart
+++ lib/src/core/ui/helpers/form_helpers.dart
@@ -4,5 +4,5 @@ import 'package:flutter/material.dart';
 void unFocus(BuildContext context)=> FocusScope.of(context).unfocus();
 
 extension unFocusExtension on BuildContext {
-  void unFocus() => Focus.of(this).unfocus(); 
+  void unFocus() => FocusScope.of(this).unfocus(); 
 }
